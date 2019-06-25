494e7aa7e219f561855f581a32cbd4d321ac7260
Samuel Flory Silbory <sam@silbory.com>
2019-06-25 14:59:27 -0700

    Add /txt to return just text

--- app.py
+++ app.py
@@ -58,5 +58,39 @@ def upload_file():
 
       return render_template("uploaded.html", displaytext=text, fname=filename)
 
+@app.route('/txt', methods = ['GET', 'POST'])
+def just_txt():
+   if request.method == 'POST':
+      f = request.files['file']
+
+      # create a secure filename
+      filename = secure_filename(f.filename)
+
+      # save file to /static/uploads
+      filepath = os.path.join(app.config['UPLOAD_FOLDER'],filename)
+      f.save(filepath)
+
+      # load the example image and convert it to grayscale
+      image = cv2.imread(filepath)
+      gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+      # apply thresholding to preprocess the image
+      gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+      # apply median blurring to remove any blurring
+      gray = cv2.medianBlur(gray, 3)
+
+      # save the processed image in the /static/uploads directory
+      ofilename = os.path.join(app.config['UPLOAD_FOLDER'],"{}.png".format(os.getpid()))
+      cv2.imwrite(ofilename, gray)
+
+      # perform OCR on the processed image
+      text = pytesseract.image_to_string(Image.open(ofilename))
+
+      # remove the processed image
+      os.remove(ofilename)
+
+      return (text)
+
 if __name__ == '__main__':
    app.run(host="0.0.0.0", port=5000, debug=True)
